cecf12a02fa01e16ae384304de0e6805dd9dada7
TinaLoh <tinalola@gmail.com>
2015-06-24 13:30:14 -0700

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root 'posts#index'
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,4 +61,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
